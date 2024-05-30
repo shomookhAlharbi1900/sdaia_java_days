2a833cbc66bfb834bb10ff78e1035520232aa4f1
Shomookh Alharbi <shomookh1900@hotmail.com>
2024-05-30 07:38:43 +0300

    add 2 class Svaing AND check

--- src/main/java/Day2/mainAccount.java
+++ src/main/java/Day2/mainAccount.java
@@ -3,11 +3,10 @@ package Day2;
 public class mainAccount {
     public static void main(String[] args) {
         Account x = new Account("Shmookh",2500);
-        SavingAccount s = new SavingAccount("jood",5000,100);
-
-        s.withdrae(100);
+        SavingAccount s = new SavingAccount("jood",5000,2000);
+        s.withdrae(1000);
         System.out.println( x.name);
-        System.out.println( s.getBalance());
+        System.out.println( s.getBalance() +  " Name :"+ s.name);
         x.depoist(500);
         System.out.println( x.getBalance());
         System.out.println( x.getmonthlyRate());
